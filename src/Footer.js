1fffe735c1d6569b5497dd4617524d8cb98dd816
yossev <synthpyt@gmail.com>
2025-05-21 13:14:42 +0200

    Fixed link

--- src/Footer.js
+++ src/Footer.js
@@ -1,7 +1,7 @@
 function Footer() {
     return (
         <footer className="footer">
-            <p className="footer-text">Made with 🤎 by <a href="https://twitter.com/yossevo" target="_blank" rel="noreferrer">Yossef</a></p>
+            <p className="footer-text">Made with 🤎 by <a href="https://github.com/yossev" target="_blank" rel="noreferrer">Yossef</a></p>
 
         </footer>
     );
